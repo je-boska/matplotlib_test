9641d05b7507e55e76e2c893e9c717436fd952d3
joneirikboska <joneirikboska@gmail.com>
2021-05-03 18:09:51 +0200

    Add third point to plotted line. Add grid and indicator for 0 axes.

--- matplottest.py
+++ matplottest.py
@@ -3,12 +3,20 @@ import numpy as np
 
 point1 = (0, 10)
 point2 = (10, 30)
+point3 = (-10, -10)
 
-x_values = (point1[0], point2[0])
-y_values = (point1[1], point2[1])
+x_values = (point1[0], point2[0], point3[0])
+y_values = (point1[1], point2[1], point3[1])
 
 plt.plot(x_values, y_values, 'bo-')
 
 plt.ylabel('y')
 plt.xlabel('x')
+
+plt.grid(b=True)
+plt.xlim(-40, 40)
+plt.ylim(-40, 40)
+
+plt.axhline(color='black', lw=1)
+plt.axvline(color='black', lw=1)
 plt.show()
