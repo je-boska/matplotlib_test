531d506c2b3f43f6304519ab1281a7cc6021dcaa
joneirikboska <joneirikboska@gmail.com>
2021-05-04 11:48:54 +0200

    Use graph to solve questions.

--- matplottest.py
+++ matplottest.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-point1 = (0, 10)
-point2 = (10, 30)
-point3 = (-10, -10)
+point1 = (0, 6)
+point2 = (1, 4)
+point3 = (2, 2)
 
 x_values = (point1[0], point2[0], point3[0])
 y_values = (point1[1], point2[1], point3[1])
@@ -14,8 +14,8 @@ plt.ylabel('y')
 plt.xlabel('x')
 
 plt.grid(b=True)
-plt.xlim(-40, 40)
-plt.ylim(-40, 40)
+plt.xlim(-10, 10)
+plt.ylim(-10, 10)
 
 plt.axhline(color='black', lw=1)
 plt.axvline(color='black', lw=1)
